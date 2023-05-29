ffc4c503b49e0b7e0d8e7b9b398faa8309d8d58b
jonusHK <positipman@gmail.com>
2023-05-29 13:38:51 +0900

    Add: Gradle 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ group = "com.bhkpo"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+val kotestVersion = "5.5.5"
+val kotestExtSpringVersion = "1.1.2"
+val kotestExtContainerVersion = "1.3.4"
+val mockkVersion = "1.13.4"
+
 repositories {
     mavenCentral()
 }
@@ -19,13 +24,23 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("it.ozimov:embedded-redis:0.7.2")
+    implementation("org.redisson:redisson-spring-boot-starter:3.21.1")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtSpringVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:$kotestExtContainerVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 

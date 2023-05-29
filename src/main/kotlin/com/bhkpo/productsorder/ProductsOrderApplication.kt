620e404c25b5a47979b41bf820525a35e5cb3f9a
jonusHK <positipman@gmail.com>
2023-05-29 13:31:08 +0900

    Add: init

---
+++ src/main/kotlin/com/bhkpo/productsorder/ProductsOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.bhkpo.productsorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductsOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductsOrderApplication>(*args)
+}
